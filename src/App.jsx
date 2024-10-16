a0307a4bc12953016b62f0a75585b12c7f9a13db
Pumthad Kaewmongkolsri <pumthad1670@gmail.com>
2024-10-16 13:20:55 +0700

    create nav bar

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,9 @@ function App() {
 
   return (
     <>
+      <div className="">
+        <Navbar />
+      </div>
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -28,6 +31,22 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+    </>
+  );
+}
+
+function Navbar() {
+  return (
+    <>
+      <nav className="">
+        <div className="flex justify-between items-center border-2">
+          <img src="./../image.png" className="p-3"></img>
+          <div className="flex gap-x-2 p-3">
+            <button className="bg-zinc-300">Log in</button>
+            <button className="bg-zinc-300"> Sign up</button>
+          </div>
+        </div>
+      </nav>
     </>
   );
 }
