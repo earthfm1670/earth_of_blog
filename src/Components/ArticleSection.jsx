4b85b6ec728f2e839916375ea02822c472b9848e
Pumthad Kaewmongkolsri <pumthad1670@gmail.com>
2024-10-21 16:24:54 +0700

    add: article section with props Components

--- src/Components/ArticleSection.jsx
+++ src/Components/ArticleSection.jsx
@@ -1,6 +1,7 @@
 import { Input } from "@/components/ui/input";
 import { Search } from "lucide-react";
 import * as React from "react";
+import { blogPosts } from "./../data/blogPosts";
 
 import {
   Select,
@@ -42,6 +43,55 @@ function ArticleSearch() {
           <Dropdown />
         </div>
       </div>
+
+      <BlogCard
+        imgLink={blogPosts[0].image}
+        category={blogPosts[0].category}
+        title={blogPosts[0].title}
+        description={blogPosts[0].description}
+        author={blogPosts[0].author}
+        date={blogPosts[0].date}
+      />
+      <BlogCard
+        imgLink={blogPosts[1].image}
+        category={blogPosts[1].category}
+        title={blogPosts[1].title}
+        description={blogPosts[1].description}
+        author={blogPosts[1].author}
+        date={blogPosts[1].date}
+      />
+      <BlogCard
+        imgLink={blogPosts[2].image}
+        category={blogPosts[2].category}
+        title={blogPosts[2].title}
+        description={blogPosts[2].description}
+        author={blogPosts[2].author}
+        date={blogPosts[2].date}
+      />
+      <BlogCard
+        imgLink={blogPosts[3].image}
+        category={blogPosts[3].category}
+        title={blogPosts[3].title}
+        description={blogPosts[3].description}
+        author={blogPosts[3].author}
+        date={blogPosts[3].date}
+      />
+      <BlogCard
+        imgLink={blogPosts[4].image}
+        category={blogPosts[4].category}
+        title={blogPosts[4].title}
+        description={blogPosts[4].description}
+        author={blogPosts[4].author}
+        date={blogPosts[4].date}
+      />
+      <BlogCard
+        imgLink={blogPosts[5].image}
+        category={blogPosts[5].category}
+        title={blogPosts[5].title}
+        description={blogPosts[5].description}
+        author={blogPosts[5].author}
+        date={blogPosts[5].date}
+      />
     </>
   );
 }
@@ -97,6 +147,45 @@ function Dropdown() {
         </SelectContent>
       </Select>
     </>
+  );
+}
+
+function BlogCard(props) {
+  return (
+    <div className="flex flex-col gap-4">
+      <a href="#" className="relative h-[212px] sm:h-[360px]">
+        <img
+          className="w-full h-full object-cover rounded-md"
+          src={props.imgLink}
+          alt="Understanding Cat Behavior: Why Your Feline Friend Acts the Way They Do"
+        />
+      </a>
+      <div className="flex flex-col">
+        <div className="flex">
+          <span className="bg-green-200 rounded-full px-3 py-1 text-sm font-semibold text-green-600 mb-2">
+            {props.category}
+          </span>
+        </div>
+        <a href="#">
+          <h2 className="text-start font-bold text-xl mb-2 line-clamp-2 hover:underline">
+            {props.title}
+          </h2>
+        </a>
+        <p className="text-muted-foreground text-sm mb-4 flex-grow line-clamp-3">
+          {props.description}
+        </p>
+        <div className="flex items-center text-sm">
+          <img
+            className="w-8 h-8 rounded-full mr-2"
+            src="https://res.cloudinary.com/dcbpjtd1r/image/upload/v1728449784/my-blog-post/xgfy0xnvyemkklcqodkg.jpg"
+            alt="Tomson P."
+          />
+          <span>{props.author}</span>
+          <span className="mx-2 text-gray-300">|</span>
+          <span>{props.date}</span>
+        </div>
+      </div>
+    </div>
   );
 }
 
